39ed83223ee69b77a4af1de8d02f3fe60b4d4d3b
tony <wen114teng@gmail.com>
2025-09-30 13:52:02 +0100

    2025/09/30

--- week1Exercises.py
+++ week1Exercises.py
@@ -27,20 +27,14 @@ print(len(fav_color))
 # 7.
 height = "2.453"
 float_height = float(height)
-print(float_height)
+print(float_height*2)
 
 # 8.
 is_monday = True
 is_sunny = False
-if is_monday:
-    is_weekend = False
-if is_sunny:
-    is_raining = False
-else:
-    is_raining = True
-print(is_weekend and is_raining)
-print(is_weekend or is_raining)
-print(not is_weekend)
+print(is_monday and is_sunny)
+print(is_monday or is_sunny)
+print(not is_sunny)
 
 # 9.
 x = 12
@@ -50,3 +44,67 @@ print(x == y)
 print(x <= 25)
 
 # 10.
+name = input("What is your name? ")
+age = input("What is your age? ")
+print("Hello " + name + ", You age is " + age)
+if int(age) > 18:
+    print("You're old enough.")
+
+# 11.
+print("Humpty Dumpty sat on a wall\nHumpty Dumpty had a GREAT fall\nAll the kings horses and all the kings men\nCouldn\’t put Humpty together again!!!")
+
+# 12.
+firstName = input("What is your first name? ")
+print("!" + f'{firstName}!'*2)
+
+# 13.
+print(10, end=" ")
+print("+", end=" ")
+print(8, end=" ")
+print("-", end=" ")
+print(5, end=" ")
+print("=", end=" ")
+print(10 + 8 - 5, end=" ")
+
+# 14.
+print("\n")
+weight = 70
+height = 1.75
+BMI = round(weight / (height**2), 2)
+print(f"Weight: {weight}kg\n", end="")
+print("Height: {}m".format(height))
+print("BMI: %.2f" % (BMI))
+
+# 15.
+name = "Henry Jones"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print("my name is", name, ", I am", age, "years old\n")
+print("my skills are")
+print("-", skill1, "(" +level1+ ")")
+print("-", skill2, "(" +level2+ ")", end=' ')
+print("-", skill3, "(" +level3+ " )\n")
+print("I am looking for a job with a salary of", lower, "-", upper, "pounds per month")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
